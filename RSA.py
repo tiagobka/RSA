9606481b1e0d465ce58ecd2b5b03fa6661955d9d
tiagobka <tiagobka@msn.com>
2018-11-14 10:01:56 -0800

    First push

---
+++ RSA.py
@@ -0,0 +1,36 @@
+import random
+class RSA:
+
+    def __init__(self):
+        pass
+        #this is a constructor
+    def generatePrimeNumber (self):
+        pass
+
+    def RSA_Algorithm(self):
+        pass
+        q =13
+        p = 5
+
+    def rabinMiller(self, num):
+        #Find n-1 = 2^k *m where k and m are integers
+        #pick a value a such that 1<a < n-1
+        s = num - 1
+        t = 0
+
+        while s % 2 == 0:
+            s = s // 2
+            t += 1
+        for trials in range(5):
+            a = random.randrange(2, num - 1)
+            v = pow(a, s, num)
+            if v != 1:
+                i = 0
+                while v != (num - 1):
+                    if i == t - 1:
+                        return False
+                    else:
+                        i = i + 1
+                        v = (v ** 2) % num
+            return True
+
