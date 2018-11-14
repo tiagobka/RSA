071347b094d3b55ebdc7f4499f96b1eeace83054
tiagobka <tiagobka@msn.com>
2018-11-14 10:56:20 -0800

    rabinMiller function complete

--- RSA.py
+++ RSA.py
@@ -12,25 +12,39 @@ class RSA:
         q =13
         p = 5
 
+    #This function checks tells if there is a probability of a number being prime
+    #if a number is composit it will return fa
     def rabinMiller(self, num):
-        #Find n-1 = 2^k *m where k and m are integers
-        #pick a value a such that 1<a < n-1
-        s = num - 1
-        t = 0
+        #Step1)Find n-1 = 2^i *m where i and m are integers
+        #Step2)pick a value a such that 1<a < n-1
+        #Step3)compute b0 = a^m(modn), if needed compute bi = (bi-1)^2
+        #---------STEP1
+        nm1 = num - 1
+        i = 1
+        m =nm1
 
-        while s % 2 == 0:
-            s = s // 2
-            t += 1
+        while m % 2 == 0:
+           m = m // 2
+           i += 1
+        #while isinstance(nm1%pow(2,i),int): It does not work
+        #    m = nm1//pow(2,i)
+        #    i +=1
+        #----------STEP1-END
+
         for trials in range(5):
             a = random.randrange(2, num - 1)
-            v = pow(a, s, num)
+            v = pow(a, m, num)#a^m (mod n), it is computed more efficiently than pow(x, y) % z)
             if v != 1:
-                i = 0
+                j = 0
                 while v != (num - 1):
-                    if i == t - 1:
-                        return False
+                    if j == i - 1:
+                        return False # it is composite
                     else:
-                        i = i + 1
+                        j = i + 1
                         v = (v ** 2) % num
-            return True
+            return True # it is likely a prime
 
+
+
+c = RSA()
+c.rabinMiller(54)
---
+++ UIRSAtool.py
@@ -0,0 +1,7 @@
+from tkinter import *
+
+class RSAInterface:
+    def __init__(self):
+        self.mainWindow = Tk()
+
+
